aa93725c5d796abf32661c7faa25073bec8efc3f
mirjam <mirjamthomet@gmail.com>
2020-05-11 14:48:13 +0200

    Project setup, add bootstrap and google font

---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/">Home</router-link>|
+      <router-link to="/projects">About</router-link>
+    </div>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import { BootstrapVue } from 'bootstrap-vue'
+import './style/custom.scss'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
