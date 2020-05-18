6771cf97d0f869c4239b5aaed2943ffae2cfc678
John Gannon <johgannon@gmail.com>
2020-05-18 13:22:59 +0200

    Open banner on title click

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-col>
+    <div class="profile-card">
+      <b-media>
+        <template v-slot:aside>
+          <img v-lazy="'../assets/me.jpg'" />
+        </template>
+
+        <h2>Media Body</h2>
+        <p>Some text</p>
+
+        <!-- b-[Optional: add media children here for nesting] -->
+      </b-media>
+    </div>
+  </b-col>
+</template>
+
+<script>
+export default {
+  name: 'ProfileCard'
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
